9a628aa12dbd070b7368d8f3ff200bf58f5e2b6d
ea8b1ac <100313790+ea8b1ac@users.noreply.github.com>
2022-02-27 13:42:16 -0500

    bot.lua

---
+++ bot.lua
@@ -0,0 +1,63 @@
+local bot = {
+        bMain = 0,
+        bWhitelist = {},
+    }
+
+    local Client = game.Players.LocalPlayer
+
+    function bot.init(settings)
+        if settings.main and settings.whitelisted then
+            bot.bWhitelist = settings.whitelisted
+            bot.bMain = settings.main
+        end
+
+        local function IsBot(UID)
+            bool = false
+
+            if Client.UserId ~= bot.bMain then
+                bool = true
+            end
+
+            return bool
+        end
+
+        local function sub()
+            local mod = {}
+
+            function mod.say(msg)
+                if IsBot(Client.UserId) then
+                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(tostring(msg), "All")
+                end
+            end
+
+            function mod.shell(f)
+                if IsBot(Client.UserId) then
+                    task.spawn(function()
+                        pcall(function()
+                            f(Client, bot.bWhitelist)
+                        end)
+                    end)
+                end
+            end
+
+            function mod.modifyHumanoid(i, v)
+                if IsBot(Client.UserId) then
+                    pcall(function()
+                        if Client.Character.Humanoid[i] then
+                            Client.Character.Humanoid[i] = v
+                        end
+                    end)
+                end
+            end
+
+            function mod.panic()
+                Client:Kick('\nPanic Activated')
+            end
+            
+            return mod
+        end
+
+        return sub()
+    end
+
+    return bot
